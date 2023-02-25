9af21966879a6b01df7e0bdcb7c58cb60e1cb238
Cheyo <103604710+cheyocodes@users.noreply.github.com>
2023-02-25 14:09:32 -0600

    add /health endpoint for healthchecks

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -119,5 +119,12 @@ def data_activities_reply(activity_uuid):
     return model['data'], 200
   return
 
+# One common convention for the healthcheck endpoint 
+# is to use the /health endpoint. You can add a new 
+# route to your Flask application like this:
+@app.route("/health", methods=['GET'])
+def healthcheck():
+  return "OK", 200
+
 if __name__ == "__main__":
   app.run(debug=True)
