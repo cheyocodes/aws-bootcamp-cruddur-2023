87f16326f52b2d489621b886b694b46b9a1587bb
Sergio <cheyocodes@gmail.com>
2023-03-02 21:53:58 +0000

    add Olga's sub-segment example

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,37 +1,49 @@
 from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
 
+# import XRay SDK libraries
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 
 class UserActivities:
-  def run(user_handle):
+  def __init__(self, xray_recorder, request):
+        self.xray_recorder = xray_recorder
+        self.request = request
 
-    # segment = xray_recorder.begin_segment('user_activities')
+  def run(self, user_handle):
+    try:
+      # Start a segment
+      segment = self.xray_recorder.begin_segment('user_activities_start')
+      segment.put_annotation('url', self.request.url)
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    model = {
-      'errors': None,
-      'data': None
-    }
-    now = datetime.now(timezone.utc).astimezone()
-
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
-    else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
-
-    dict = {
-      "now": now.isoformat(), 
-      "results_size": len(model['data'])
-    }
-
-    # subsegment = xray_recorder.begin_subsegment('mock-data')
-    # segment.put_metadata('key', dict, 'namespace')
+      now = datetime.now(timezone.utc).astimezone()
+      # Add metadata or annotation here if necessary
+      xray_dict = {'now': now.isoformat()}
+      segment.put_metadata('now', xray_dict, 'user_activities')
+      segment.put_metadata('method', self.request.method, 'http')
+      segment.put_metadata('url', self.request.url, 'http')
+      if user_handle == None or len(user_handle) < 1:
+        model['errors'] = ['blank_user_handle']
+      else:
+        # Start a subsegment
+        subsegment = self.xray_recorder.begin_subsegment('user_activities_nested_subsegment')
+        now = datetime.now()
+        results = [{
+          'uuid': '<put your own id>',
+          'handle':  'White Unicorn',
+          'message': 'Cloud is fun!',
+          'created_at': (now - timedelta(days=1)).isoformat(),
+          'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
+        xray_dict['results'] = len(model['data'])
+        subsegment.put_metadata('results', xray_dict, 'user_activities')
+    finally:  
+      # Close the segment
+      self.xray_recorder.end_subsegment()
     return model
